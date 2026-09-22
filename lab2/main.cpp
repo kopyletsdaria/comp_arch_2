737919571fe3708247972197120d6ad578164c31
kopyletsdaria <kopylets.daria@lll.kpi.ua>
2026-09-22 18:48:09 +0300

    Add files via upload

---
+++ lab2/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "calculator.h"
+using namespace std;
+int main() {
+    Calculator calc;
+ cout << "Add(10.5, 2.3) = " << calc.Add(10.5, 2.3) << endl;
+ cout << "Sub(10.5, 2.3) = " << calc.Sub(10.5, 2.3) << endl;
+    return 0;
+}
